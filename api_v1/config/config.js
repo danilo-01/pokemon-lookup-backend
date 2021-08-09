19291359378a89f858c9fef8effceffd7d37cdc2
danilo-01 <66889136+danilo-01@users.noreply.github.com>
2021-08-09 18:59:49 -0500

    Updated config file

--- api_v1/config/config.js
+++ api_v1/config/config.js
@@ -10,8 +10,8 @@ const BCRYPT_WORK_FACTOR = 10;
 
 const DB_URI =
   process.env.NODE_ENV === 'test'
-    ? 'postgresql:///bankly_test'
-    : 'postgresql:///bankly';
+    ? 'postgresql:///poke_connect'
+    : 'postgresql:///poke_connect';
 
 module.exports = {
   BCRYPT_WORK_FACTOR,
